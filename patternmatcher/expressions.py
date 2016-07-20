a6924f8265706e3294095b608f1357b86420a4c0
Wheerd <admin@wheerd.de>
2016-07-20 18:59:53 +0200

    Added some more documentation and cleaned up a bit.

--- patternmatcher/expressions.py
+++ patternmatcher/expressions.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 import math
-from typing import Optional, Any, Tuple, Dict, Callable
-from enum import Enum
 from collections import Counter as Multiset
-from .utils import isidentifier
+from enum import Enum
+from typing import Any, Callable, Dict, Optional, Tuple
+
+from patternmatcher.utils import isidentifier
+
 
 class Arity(tuple, Enum):
     """Arity of an operator as (`int`, `int`) tuple.
@@ -55,9 +57,8 @@ class Expression(object):
         self.variables = Multiset() # type: Multiset[str]
         self.constraint = constraint
 
-    # TODO: call it canonicalize maybe?
     def simplify(self):
-        """Simplifies the expression using properties like operation associativity or """
+        """Simplifies the expression using properties like operation associativity, etc."""
         return self
 
     @property
@@ -76,33 +77,53 @@ class Expression(object):
         return self.is_constant or self.variables.most_common(1)[0][1] == 1
 
 class Operation(Expression):
-    """Base class for all operations.
+    """Base class for all operations."""
 
-    Attributes:
-        name
-            Name or symbol for the operator.
-        arity
-            The arity of the operator as (`int`, `int`) tuple. The first component represents
-            the minimum number of operands, the second the maximum number. See the :class:`Arity` enum.
-        associative
-            True if the operation is associative, i.e. `f(a, f(b, c)) = f(f(a, b), c)`.
-            This property is used to flatten nested associative operations of the same type.
-            Therefore, the `arity` of an associative operation has to have an unconstraint maximum
-            number of operand. 
-        commutative
-            True if the operation is commutative, i.e. `f(a, b) = f(b, a)`. Note that commutative
-            operations will always be converted into canonical form with sorted operands.
-        one_identity
-            True if the operation with a single argument is equivalent to the identity function, i.e.
-            `f(a) = a`. This property is used to simplify expressions.
-    """
     name = None # type: str
+    """Name or symbol for the operator."""
+
     arity = Arity.variadic # type: Tuple[int, int]
+    """The arity of the operator as (`int`, `int`) tuple.
+
+    The first component represents
+    the minimum number of operands, the second the maximum number. See the :class:`Arity` enum.
+    """
+
     associative = False
+    """True if the operation is associative, i.e. `f(a, f(b, c)) = f(f(a, b), c)`.
+    
+    This property is used to flatten nested associative operations of the same type.
+    Therefore, the `arity` of an associative operation has to have an unconstraint maximum
+    number of operand. 
+    """
+
     commutative = False
+    """True if the operation is commutative, i.e. `f(a, b) = f(b, a)`.
+    
+    Note that commutative operations will always be converted into canonical
+    form with sorted operands.
+    """
+
     one_identity = False
+    """True if the operation with a single argument is equivalent to the identity function.
+    
+    This property is used to simplify expressions, e.g. for `f` with `f.one_identity = True`
+    the expression `f(a)` if simplified to `a`.
+    """
 
     def __init__(self, *operands: Expression, constraint:Optional[Constraint]=None) -> None:
+        """Base class for all expressions.
+
+        All expressions are immutable, i.e. their attributes should not be changed,
+        as several attributes are computed at instantiation and are not refreshed.
+
+        Arguments:
+            operands
+                The operands for the operation expression.
+            constraint
+                An optional constraint expression, which is checked for each match
+                to verify it.
+        """
         super().__init__(constraint)
         self.operands = list(operands)
         self.constraint = constraint
@@ -187,21 +208,46 @@ class Variable(Atom):
         return '%s_: %s' % (self.name, self.expression)
 
 class Wildcard(Atom):
+    """A wildcard that matches any expression.
+    
+    The wildcard will match any number of expressions between `min_count` and `max_count`.
+    Optionally, the wildcard can also be constrained to only match expressions satisfying a predicate.
+    """
+
     def __init__(self, min_count: int, max_count: int, constraint: Optional[Constraint]=None) -> None:
+        """
+        Arguments:
+            min_count
+                The minimum number of expressions this wildcard will match. Must not be
+                greater than `max_count` or negative.
+            max_count
+                The maximum number of expressions this wildcard will match. Use `math.inf`
+                for an unbounded wildcard. Must not be smaller than `min_count`.
+            constraint
+                An optional constraint for expressions to be considered a match. If set, this
+                callback is invoked for every match and the return value is utilized to decide
+                whether the match is valid.
+        """
+        assert min_count >= 0, 'Negative min_count'
+        assert min_count <= max_count, 'min_count > max_count'
+
         super().__init__(constraint)
         self.min_count = min_count
         self.max_count = max_count
 
     @staticmethod
     def dot():
+        """Creates a :class:`Wildcard` that matches exactly one argument."""
         return Wildcard(min_count=1, max_count=1)
 
     @staticmethod
     def star():
+        """Creates a :class:`Wildcard` that matches any number of arguments."""
         return Wildcard(min_count=0, max_count=math.inf)
 
     @staticmethod
     def plus():
+        """Creates a :class:`Wildcard` that matches at least one and up to any number of arguments."""
         return Wildcard(min_count=1, max_count=math.inf)
 
     def __str__(self):
