8a3284c031526c447be8dac62973aec58cfe2c7d
Wheerd <admin@wheerd.de>
2016-08-03 17:31:18 +0200

    Another unfinished state.

--- patternmatcher/syntactic.py
+++ patternmatcher/syntactic.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 import itertools
 import math
+
 from graphviz import Digraph
 
-from patternmatcher.expressions import Variable, Operation, Arity, Symbol, Wildcard, Atom
+from patternmatcher.expressions import (Arity, Atom, Operation, Symbol,
+                                        Variable, Wildcard)
+
 
 class OperationEnd(object):
     def __str__(self):
@@ -11,7 +14,14 @@ class OperationEnd(object):
 
 OPERATION_END = OperationEnd()
 
+def is_operation(term):
+    return isinstance(term, type) and issubclass(term, Operation)
+
 def flatterm_iter(expression):
+    """Generator that yields the atoms of the expressions in prefix notation with operation end markers.
+    
+    See :class:`Flatterm` for details of the flatterm form.
+    """
     if isinstance(expression, Variable):
         yield from flatterm_iter(expression.expression)
     elif isinstance(expression, Operation):
@@ -24,12 +34,27 @@ def flatterm_iter(expression):
     else:
         raise TypeError()
 
+def combined_wildcards_iter(flatterm):
+    """Combines consecutive wildcards in a flatterm into a single one"""
+    last_wildcard = None
+    for term in flatterm:
+        if isinstance(term, Wildcard):
+            if last_wildcard is not None:
+                last_wildcard = Wildcard(last_wildcard.min_count + term.min_count, last_wildcard.max_count + term.max_count)
+            else:
+                last_wildcard = term
+        else:
+            if last_wildcard is not None:
+                yield last_wildcard
+                last_wildcard = None
+            yield term
+
 class Flatterm(list):
     def __init__(self, expression):
         list.__init__(self, flatterm_iter(expression))
 
     def _term_str(self, term):
-        if isinstance(term, type) and issubclass(term, Operation):
+        if is_operation(term):
             return term.name + '('
         elif isinstance(term, Wildcard):
             return '*[%s,%s]' % (term.min_count, term.max_count)
@@ -39,189 +64,157 @@ class Flatterm(list):
     def __str__(self):
         return ' '.join(map(self._term_str, self))
 
-def get_flatterm(expression):
-    if isinstance(expression, Variable):
-        return get_flatterm(expression.expression)
-    if isinstance(expression, Operation):
-        name = expression.name + '('
-        return list(itertools.chain([name], \
-            itertools.chain.from_iterable(map(get_flatterm, expression.operands)), \
-            [')']))
-    if isinstance(expression, Symbol):
-        return [expression.name]
-    if isinstance(expression, Wildcard):
-        # TODO: What about non-inf max with 0 min?
-        wc = '*' * expression.min_count + (expression.max_count == math.inf and '+' or '')
-        return [wc]
-
-    raise NotImplementedError()
-
-def is_wildcard(term):
-    return term[0] == '*' and (term[-1] == '*' or term[-1] == '+')
-
-def combine_wildcards(flattterm):
-    flat = []
-
-    for expr in flattterm:
-        try:
-            last = flat[-1]
-            was_wildcard = is_wildcard(last)
-            next_wildcard = is_wildcard(expr)
-
-            if was_wildcard and next_wildcard:
-                unbounded = last[-1] == '+' or expr[-1] == '+'
-                min_count = (last[-1] != '+' and len(last) or len(last) - 1) + \
-                            (expr[-1] != '+' and len(expr) or len(expr) - 1)
-                new_wildcard = ('*' * min_count) + (unbounded and '+' or '')
-
-                flat[-1] = new_wildcard
-            else:
-                flat.append(expr)
-        except IndexError:
-            flat.append(expr)
-
-    return flat
+class _WildcardState:
+    def __init__(self):
+        self.last_wildcard = None
+        self.symbol_after = None
+        self.all_same = True
+        self.fail_node = None
 
 def generate_net(pattern):
+    """"""
     last_node = None
     last_term = None
-    last_wildcard = [None]
-    last_symbol_after_wildcard = [None]
-    fail_nodes = [None]
-    all_same = [True]
+    # Capture the last unbounded wildcard for every level of operation nesting on a stack
+    # Used to add backtracking edges in case the "match" fails later 
+    wildcard_states = [_WildcardState()]
     root = node = Node()
-    root.id = 1
-    flatterm = list(combine_wildcards(get_flatterm(pattern)))
+    flatterm = list(combined_wildcards_iter(flatterm_iter(pattern)))
 
-    i = 2
+    #i = 2
     for j, term in enumerate(flatterm):
         last_node = node
         last_term = term
-        if is_wildcard(term):
-            for t in term:
-                if t == '+':
-                    node['*'] = node
-                else:
-                    node['*'] = Node()
-                    node['*'].id = i
-                    i += 1
-                    node = node['*']
-            if term[-1] == "+":
-                last_wildcard[-1] = node
-                last_symbol_after_wildcard[-1] = None
-                all_same[-1] = True
+        state = wildcard_states[-1]
+        # For wildcards, generate a chain of #min_count Wildcard edges
+        # If the wildcard is unbounded (max_count = math.inf),
+        # add a wildcard self loop at the end
+        if isinstance(term, Wildcard):
+            for _ in range(term.min_count):
+                node[Wildcard] = Node()
+                node = node[Wildcard]
+            if term.max_count == math.inf:
+                node[Wildcard] = node
+                # set wildcard state to this reference this wildcard
+                state.last_wildcard = node
+                state.symbol_after = None
+                state.all_same = True
         else:
             node[term] = Node()
-            node[term].id = i
-            i += 1
             node = node[term]
-            if last_symbol_after_wildcard[-1] is None:
-                last_symbol_after_wildcard[-1] = term
-            if term != last_symbol_after_wildcard[-1]:
-                all_same[-1] = False
-            if term[-1] == '(':
-                last_wildcard.append(None)
-                last_symbol_after_wildcard.append(None)
-                fail_nodes.append(None)
-                all_same.append(True)
-            if term == ')':
-                last_wildcard.pop()
-                last_symbol_after_wildcard.pop()
-                fail_nodes.pop()
-                all_same.pop()
-            if last_wildcard[-1] is not None:
-                try:
-                    next_term = flatterm[j+1]
-                except IndexError:
-                    next_term = None
-                if next_term is not None and not is_wildcard(next_term):
-                    node['*'] = last_wildcard[-1]
-                    if next_term != last_symbol_after_wildcard[-1]:
-                        if all_same[-1] and next_term == ')':
-                            node[last_symbol_after_wildcard[-1]] = node
+            if state.symbol_after is None:
+                state.symbol_after = term
+            if term != state.symbol_after:
+                state.all_same = False
+            if is_operation(term):
+                wildcard_states.append(_WildcardState())
+            if term == OPERATION_END:
+                wildcard_states.pop()
+
+            state = wildcard_states[-1]
+            try:
+                next_term = flatterm[j+1]
+            except IndexError:
+                next_term = None
+
+            # Potentially, backtracking wildcard edges have to be added
+            if next_term is not None and not isinstance(next_term, Wildcard):
+                # If there was an unbounded wildcard inside the current operation,
+                # add a backtracking wildcard edge to it
+                if state.last_wildcard is not None:
+                    node[Wildcard] = state.last_wildcard
+                    # Also add an edge for the symbol directly after the wildcard to
+                    # its respecitive node (or as a self loop if all symbols are the same)
+                    if next_term != state.symbol_after:
+                        if state.all_same and next_term == OPERATION_END:
+                            node[state.symbol_after] = node
                         else:
-                            node[last_symbol_after_wildcard[-1]] = last_wildcard[-1][last_symbol_after_wildcard[-1]]
-            elif any(x is not None for x in last_wildcard):
-                if fail_nodes[-1] is None:
-                    fail_nodes[-1] = fn = Node()
-                    fn['*'] = fn
-                    for w in reversed(last_wildcard[:-1]):
-                        fn[')'] = w or Node()
-                        if w is None:
-                            fn = fn[')']
-                            fn['*'] = fn
-                        else:
-                            break
-                node['*'] = fail_nodes[-1]
+                            node[state.symbol_after] = state.last_wildcard[state.symbol_after]
+                # If there was an unbounded wildcard inside a parent operation of the current one,
+                # an additional fail state is needed, that eventually backtracks to the wildcard
+                # Every level of operation nesting gets its own fail node until the level of the
+                # wildcard is reached
+                elif any(s.last_wildcard is not None for s in wildcard_states):
+                    if state.fail_node is None:
+                        state.fail_node = fn = Node()
+                        fn[Wildcard] = fn
+                        for last_state in reversed(wildcard_states[:-1]):
+                            fn[OPERATION_END] = last_state.last_wildcard or last_state.fail_node or Node()
+                            if last_state.fail_node is not None or last_state.last_wildcard is not None:
+                                break
+                            last_state.fail_node = fn = fn[OPERATION_END]
+                            fn[Wildcard] = fn
+                    node[Wildcard] = state.fail_node
 
     last_node[last_term] = [pattern]
 
     return root
 
+class _NodeQueueItem(object):
+    def __init__(self, node1, node2, id1, id2):
+        self.node1 = node1
+        self.node2 = node2
+        self.id1 = id1
+        self.id2 = id2
+        self.depth = 0
+        self.fixed = 0
+
+    @property
+    def keys(self):
+        keys = set()
+        if self.node1 is not None:
+            keys.update(self.node1.keys())
+        if self.node2 is not None:
+            keys.update(self.node2.keys())
+        return keys
+
 def product_net(node1, node2):
+    def get_keys_and_ids(*nodes):
+        keys = set()
+        ids = []
+        for node in nodes:
+            if node is not None:
+                keys.update(node.keys())
+                ids.append(node.id)
+            else:
+                ids.append(0)
+
+        return keys, tuple(ids)
+
+    def get_child_with_id(node, key):
+        if node is not None:
+            try:
+                try:
+                    child = node[key]
+                except (KeyError):
+                    if key == OPERATION_END:
+                        return None, 0
+                    child = node[Wildcard]
+                return child, child.id
+            except KeyError:
+                return None, 0
+            except AttributeError:
+                return child, 0
+        return None, 0
+
     root = Node()
     nodes = {(node1.id, node2.id): root}
     queue = [(node1, node2)]
     
     while len(queue) > 0:
         n1, n2 = queue.pop(0)
-        keys = set()
-        if n1 is not None:
-            keys.update(n1.keys())
-            id1 = n1.id
-        else:
-            id1 = 0
-        if n2 is not None:
-            keys.update(n2.keys())
-            id2 = n2.id
-        else:
-            id2 = 0
-
-        keys = list(keys)
-        keys.sort()
+        keys, (id1, id2) = get_keys_and_ids(n1, n2)
 
         node = nodes[(id1, id2)]
 
         for k in list(keys):
-            if n1 is not None:
-                try:
-                    try:
-                        t1 = n1[k]
-                    except (KeyError):
-                        t1 = n1['*']
-                        if k == ')':
-                            raise KeyError()
-                    id1 = t1.id
-                except KeyError:
-                    t1 = None
-                    id1 = 0
-                except AttributeError:
-                    id1 = 0
-            else:
-                t1 = None
-                id1 = 0
-            if n2 is not None:
-                try:
-                    try:
-                        t2 = n2[k]
-                    except (KeyError):
-                        if k == ')':
-                            raise KeyError()
-                        t2 = n2['*']
-                    id2 = t2.id
-                except KeyError:
-                    t2 = None
-                    id2 = 0
-                except AttributeError:
-                    id2 = 0
-            else:
-                t2 = None
-                id2 = 0
+
+            t1, id1 = get_child_with_id(n1, k)
+            t2, id2 = get_child_with_id(n2, k)
             
             if id1 != 0 or id2 != 0:
                 if (id1, id2) not in nodes:
                     nt = Node()
-                    nt.id = 100 * id1 + id2
                     nodes[(id1, id2)] = nt
                     queue.append((t1, t2))
                 
@@ -254,6 +247,14 @@ class DiscriminationNet(object):
     def match(self, expression):
         pass
 
+    def _term_str(self, term):
+        if is_operation(term):
+            return term.name + '('
+        elif term == Wildcard:
+            return '*'
+        else:
+            return str(term)
+
     def dot(self):
         dot = Digraph()
 
@@ -282,11 +283,11 @@ class DiscriminationNet(object):
 
             for (label, other) in node.items():
                 if isinstance(other, Node):
-                    dot.edge('n%s' % node.id, 'n%s' % other.id, label)
+                    dot.edge('n%s' % node.id, 'n%s' % other.id, self._term_str(label))
                     if other.id not in nodes:
                         queue.append(other)
                 else:
-                    dot.edge('n%s' % node.id, 'l%s' % id(other), label)
+                    dot.edge('n%s' % node.id, 'l%s' % id(other), self._term_str(label))
 
         return dot
 
@@ -308,10 +309,8 @@ if __name__ == '__main__':
     expr3 = f(g(a))
     expr4 = f(a, z)
 
-    ft = Flatterm(f(z, g(a)))
-    print(ft)
+    # EDGE CASE: f(z, a, x, b)
 
-    #print (' '.join(str(x) for x in combine_wildcards(get_flatterm(expr))))
     net = DiscriminationNet()
 
     net1 = generate_net(expr1)
@@ -319,11 +318,8 @@ if __name__ == '__main__':
     net3 = generate_net(expr3)
     net4 = generate_net(expr4)
 
-    net5 = product_net(net2, net3)
+    net5 = product_net(net1, net4)
     net._net = net5 # product_net2(net4, net3)
-
-    #net.add(expr1)
-    #net.add(expr2)
 
     graph = net.dot()
     #print(graph.source)
