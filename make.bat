85a4cd40809208c8a0c2d43b2f8d3ea9c467b497
Wheerd <admin@wheerd.de>
2016-08-20 16:51:38 +0200

    Added make.bat and fixed coverage make task.

--- make.bat
+++ make.bat
@@ -1,13 +1,21 @@
-init:
+@ECHO off    
+if /I %1 == init goto :init
+if /I %1 == test goto :test
+if /I %1 == check goto :check
+if /I %1 == coverage goto :coverage
+
+goto :eof
+
+:init
 	pip install -r requirements.txt
 
-test:
+:test
 	python -m unittest discover tests
 
-check:
+:check
 	pylint patternmatcher
 
-coverage:
+:coverage
     coverage run -m --branch tests.test_syntactic
     coverage run -a -m --branch tests.test_functions
     coverage run -a -m --branch tests.test_utils
