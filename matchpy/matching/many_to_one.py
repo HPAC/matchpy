c66c0480f1772c40731b270ac851dd5cb64f9149
Manuel Krebber <admin@wheerd.de>
2017-01-17 12:12:18 +0100

    Added basic secondary automaton implementation.

--- matchpy/matching/many_to_one.py
+++ matchpy/matching/many_to_one.py
@@ -23,6 +23,8 @@ f(y_, b) matched with {y ↦ a}
 """
 
 import math
+from itertools import groupby
+from operator import itemgetter
 from collections import deque
 from typing import (Container, Dict, Iterable, Iterator, List, NamedTuple, Optional, Sequence, Set, Tuple, Type, Union)
 
@@ -581,3 +583,66 @@ class CommutativeMatcher(object):
                     return False
                 last_pattern = next_pattern
         return True
+
+class SecondaryAutomaton():
+
+    def __init__(self, k):
+        self.k = k
+        self.states = self._build(k)
+
+    def match(self, edges):
+        raise NotImplementedError
+
+    @staticmethod
+    def _build(k):
+        states = dict()
+        queue = [frozenset([0])]
+
+        while queue:
+            state_id = queue.pop(0)
+            state = states[state_id] = dict()
+            for i in range(1, 2**k):
+                new_state = set()
+                for t in [2**j for j in range(k) if i & 2**j]:
+                    for v in state_id:
+                        new_state.add(t | v)
+                new_state = frozenset(new_state - state_id)
+                if new_state:
+                    if new_state != state_id:
+                        state[i] = new_state
+                    if new_state not in states and new_state not in queue:
+                        queue.append(new_state)
+
+        keys = sorted(states.keys())
+        new_states = []
+
+        for state in keys:
+            new_states.append(states[state])
+
+        for i in range(len(new_states)):
+            state = new_states[i]
+            new_state = dict()
+            for key, value in state.items():
+                new_state[key] = keys.index(value)
+            new_states[i] = new_state
+            print(new_state)
+
+        return new_states
+
+    def as_graph(self):
+        graph = Digraph()
+        for i in range(len(self.states)):
+            #node_id = '-'.join(map(str, sorted(state)))
+            #label = '\n'.join(bin(l)[2:].zfill(k) for l in sorted(state))
+            graph.node(str(i), str(i))
+
+        for state, edges in enumerate(self.states):
+            #node_id = '-'.join(map(str, sorted(state)))
+            for target, labels in groupby(sorted(edges.items()), key=itemgetter(1)):
+                #target_id = '-'.join(map(str, sorted(target)))
+                label = '\n'.join(bin(l)[2:].zfill(self.k) for l, _ in labels)
+                graph.edge(str(state), str(target), label)
+
+        return graph
+
+
