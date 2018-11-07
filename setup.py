1782a047a8452ae2a030b09423317832d0cd86c0
Sergey B Kirpichev <skirpichev@gmail.com>
2018-11-08 00:09:22 +0300

    Use setup.cfg for setup() options
    see
    https://setuptools.readthedocs.io/en/latest/setuptools.html#configuring-setup-using-setup-cfg-files
    
    Also this commit expands some metadata, e.g. classifiers and
    project_urls.  Testing requirements are specified with "tests" extras.
    Other development requirements are tracked with "develop" extras.
    
    References to extras are supported in tests_require since setuptools
    v36.7.0, hence additional entry in setup_requires.

--- setup.py
+++ setup.py
@@ -1,48 +1,6 @@
 # -*- coding: utf-8 -*-
-import os.path
 
-from setuptools import setup, find_packages
-
-root = os.path.dirname(__file__)
-
-with open(os.path.join(root, 'README.rst'), encoding='utf-8') as f:
-    readme = f.read()
+import setuptools
 
 
-setup(
-    name="matchpy",
-    use_scm_version=True,
-    description="A library for pattern matching on symbolic expressions.",
-    long_description=readme,
-    author="Manuel Krebber",
-    author_email="admin@wheerd.de",
-    url='https://github.com/HPAC/matchpy',
-    license='MIT',
-    zip_safe=True,
-    packages=find_packages(exclude=('tests', )),
-    test_suite='tests',
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-    ],
-    setup_requires=[
-        'setuptools_scm >= 1.7.0',
-        'pytest-runner',
-    ],
-    tests_require=[
-        'pytest',
-        'hypothesis',
-    ],
-    install_requires=[
-        'hopcroftkarp>=1.2,<2.0',
-        'multiset>=2.0,<3.0',
-    ],
-    extras_require={
-        'graphs': ['graphviz>=0.5,<0.6'],
-    },
-)
-
+setuptools.setup()
