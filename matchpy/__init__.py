6d196e70df4aabef5ded380da6ba2dde9c0d8732
Henrik Barthels <barthels@aices.rwth-aachen.de>
2019-04-11 09:31:48 +0200

    Set up versioneer.

--- matchpy/__init__.py
+++ matchpy/__init__.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 """Contains all the necessary classes and functions for pattern matching."""
 
-import pkg_resources
-
 # pylint: disable=wildcard-import
 from . import expressions
 from . import functions
@@ -16,4 +14,6 @@ from .matching import *
 
 __all__ = expressions.__all__ + functions.__all__ + utils.__all__ + matching.__all__
 
-__version__ = pkg_resources.get_distribution(__name__).version
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
--- setup.py
+++ setup.py
@@ -1,6 +1,1 @@
 # -*- coding: utf-8 -*-
-
-import setuptools
-
-
-setuptools.setup(use_scm_version=True)
