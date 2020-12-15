cb9671dd55a03f433e7d562d68e65ad05c476150
Francesco Bonazzi <franz.bonazzi@gmail.com>
2020-12-15 23:24:08 +0100

    Addeding sort_key to Hopcroft-Karp in order to make result of Hopcroft-Karp algorithm independent of hashing.

--- matchpy/matching/hopcroft_karp.py
+++ matchpy/matching/hopcroft_karp.py
@@ -12,57 +12,69 @@ class HopcroftKarp(Generic[TLeft, TRight]):
 
     The bipartite graph has types TLeft and TRight on the two partitions.
 
-    The constructor accepts a `map` mapping the left vertices to the set of
+    The constructor accepts a `dict` mapping the left vertices to the set of
     connected right vertices.
 
-    The method `.hopcroft_karp()` finds the maximum cardinality matching,
-    returning its cardinality. The matching will be stored in the file
-    `pair_left`
-    and `pair_right` after the matching is found.
+    The method `.hopcroft_karp()` (internally) finds the maximum cardinality
+    matching, returning its cardinality. An instance of maximum matching may be
+    returned by `.get_maximum_matching()`, while `.get_maximum_matching_num()`
+    returns both cardinality and an instance of maximum matching.
     """
 
     def __init__(self, _graph_left: Dict[TLeft, Set[TRight]]):
-        self._graph_left: Dict[TLeft, Set[TRight]] = _graph_left
+
+        def sorting_key(x):
+            return tuple(ord(i) for i in str(x))
+
+        self._pos2left: List[TLeft] = sorted(set(_graph_left), key=sorting_key)
+        self._pos2right: List[TRight] = sorted({j for i in _graph_left.values() for j in i}, key=sorting_key)
+        map_left2pos = {e: i for i, e in enumerate(self._pos2left)}
+        map_right2pos = {e: i for i, e in enumerate(self._pos2right)}
+        # Convert the graph to integers:
+        self._graph_left: Dict[int, Set[int]] = {map_left2pos[k]: {map_right2pos[j] for j in v} for k, v in _graph_left.items()}
         self._reference_distance = INT_MAX
-        self.pair_left: Dict[TLeft, TRight] = {}
-        self.pair_right: Dict[TRight, TLeft] = {}
-        self._left: List[TLeft] = []
-        self._dist_left: Dict[TLeft, int] = {}
-        self._get_left_indices_vector(_graph_left)
+        self._pair_left: Dict[int, int] = {}
+        self._pair_right: Dict[int, int] = {}
+        self._left: List[int] = []
+        self._dist_left: Dict[int, int] = {}
+        self._get_left_indices_vector(self._graph_left)
 
     def hopcroft_karp(self) -> int:
-        self.pair_left.clear()
-        self.pair_right.clear()
+        self._pair_left.clear()
+        self._pair_right.clear()
         self._dist_left.clear()
-        left: TLeft
+        left: int
         for left in self._left:
             self._dist_left[left] = INT_MAX
         matchings: int = 0
         while True:
             if not self._bfs_hopcroft_karp():
                 break
-            left: TLeft
             for left in self._left:
-                if left in self.pair_left:
+                if left in self._pair_left:
                     continue
                 if self._dfs_hopcroft_karp(left):
                     matchings += 1
         return matchings
 
     def get_maximum_matching(self) -> Dict[TLeft, TRight]:
-        self.hopcroft_karp()
-        return self.pair_left
+        matchings, maximum_matching = self.get_maximum_matching_num()
+        return maximum_matching
 
-    def _get_left_indices_vector(self, m: Dict[TLeft, Set[TRight]]) -> None:
-        p: Tuple[TLeft, Set[TRight]]
-        for p in m.items():
-            self._left.append(p[0])
+    def get_maximum_matching_num(self) -> Tuple[int, Dict[TLeft, TRight]]:
+        matchings = self.hopcroft_karp()
+        return matchings, {self._pos2left[k]: self._pos2right[v] for k, v in self._pair_left.items()}
+
+    def _get_left_indices_vector(self, m: Dict[int, Set[int]]) -> None:
+        p: int
+        for p in m:
+            self._left.append(p)
 
     def _bfs_hopcroft_karp(self) -> bool:
-        vertex_queue: List[TLeft] = []
-        left_vert: TLeft
+        vertex_queue: List[int] = []
+        left_vert: int
         for left_vert in self._left:
-            if left_vert not in self.pair_left:
+            if left_vert not in self._pair_left:
                 vertex_queue.append(left_vert)
                 self._dist_left[left_vert] = 0
             else:
@@ -71,34 +83,34 @@ class HopcroftKarp(Generic[TLeft, TRight]):
         while True:
             if len(vertex_queue) == 0:
                 break
-            left_vertex: TLeft = vertex_queue.pop(0)
+            left_vertex: int = vertex_queue.pop(0)
             if self._dist_left[left_vertex] >= self._reference_distance:
                 continue
-            right_vertex: TRight
+            right_vertex: int
             for right_vertex in self._graph_left[left_vertex]:
-                if right_vertex not in self.pair_right:
+                if right_vertex not in self._pair_right:
                     if self._reference_distance == INT_MAX:
                         self._reference_distance = self._dist_left[left_vertex] + 1
                 else:
-                    other_left: TLeft = self.pair_right[right_vertex]
+                    other_left: int = self._pair_right[right_vertex]
                     if self._dist_left[other_left] == INT_MAX:
                         self._dist_left[other_left] = self._dist_left[left_vertex] + 1
                         vertex_queue.append(other_left)
         return self._reference_distance < INT_MAX
 
-    def _swap_lr(self, left: TLeft, right: TRight) -> None:
-        self.pair_left[left] = right
-        self.pair_right[right] = left
+    def _swap_lr(self, left: int, right: int) -> None:
+        self._pair_left[left] = right
+        self._pair_right[right] = left
 
-    def _dfs_hopcroft_karp(self, left: TLeft) -> bool:
-        right: TRight
+    def _dfs_hopcroft_karp(self, left: int) -> bool:
+        right: int
         for right in self._graph_left[left]:
-            if right not in self.pair_right:
+            if right not in self._pair_right:
                 if self._reference_distance == self._dist_left[left] + 1:
                     self._swap_lr(left, right)
                     return True
             else:
-                other_left: TLeft = self.pair_right[right]
+                other_left: int = self._pair_right[right]
                 if self._dist_left[other_left] == self._dist_left[left] + 1:
                     if self._dfs_hopcroft_karp(other_left):
                         self._swap_lr(left, right)
--- tests/test_hopcroft_karp.py
+++ tests/test_hopcroft_karp.py
@@ -19,29 +19,29 @@ class TestHopcroftKarp:
         }
         expected: Dict[int, str] = {0: "v1", 1: "v4", 2: "v2", 3: "v0", 4: "v3"}
         hk = HopcroftKarp[int, str](graph)
-        matchings = hk.hopcroft_karp()
-        assert hk.pair_left == expected
+        matchings, maximum_matching = hk.get_maximum_matching_num()
+        assert maximum_matching == expected
         assert matchings == 5
 
         graph: Dict[str, Set[int]] = {'A': {1, 2}, 'B': {2, 3}, 'C': {2}, 'D': {3, 4, 5, 6},
                                       'E': {4, 7}, 'F': {7}, 'G': {7}}
         expected: Dict[str, int] = {'A': 1, 'B': 3, 'C': 2, 'D': 5, 'E': 4, 'F': 7}
         hk = HopcroftKarp[str, int](graph)
-        matchings = hk.hopcroft_karp()
-        assert hk.pair_left == expected
+        matchings, maximum_matching = hk.get_maximum_matching_num()
+        assert maximum_matching == expected
         assert matchings == 6
 
         graph: Dict[int, Set[str]] = {1: {'a': 'c'}, 2: {'a', 'c'}, 3: {'c', 'b'}, 4: {'e'}}
         expected: Dict[int, str] = {1: 'a', 2: 'c', 3: 'b', 4: 'e'}
         hk = HopcroftKarp[int, str](graph)
-        matchings = hk.hopcroft_karp()
-        assert hk.pair_left == expected
+        matchings, maximum_matching = hk.get_maximum_matching_num()
+        assert maximum_matching == expected
         assert matchings == 4
 
         graph: Dict[str, Set[int]] = {'A': {3, 4}, 'B': {3, 4}, 'C': {3}, 'D': {1, 5, 7},
                                       'E': {1, 2, 7}, 'F': {2, 8}, 'G': {6}, 'H': {2, 4, 8}}
         expected: Dict[str, int] = {'A': 3, 'B': 4, 'D': 1, 'E': 7, 'F': 8, 'G': 6, 'H': 2}
         hk = HopcroftKarp[str, int](graph)
-        matchings = hk.hopcroft_karp()
-        assert hk.pair_left == expected
+        matchings, maximum_matching = hk.get_maximum_matching_num()
+        assert maximum_matching == expected
         assert matchings == 7
